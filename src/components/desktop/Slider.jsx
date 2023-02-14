03ce30225f3f822ffb37fe96cb8c96f761e90393
Agustin <araneoagustin@gmail.com>
2023-02-14 15:48:51 -0300

    fix problema al visualizar las imagenes del slider,y  creacion de button mas info, que redirecciona

--- src/components/desktop/Slider.jsx
+++ src/components/desktop/Slider.jsx
@@ -1,31 +1,35 @@
-/* eslint-disable @next/next/no-img-element */
-import React from "react";
-import { useRef, useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import images from "../images"
+import images from "../images";
+import Link from "next/link";
+import Image from "next/image";
 
 const Slider = () => {
-const [width, setWidth] = useState(0)
-const carousel = useRef()
-
-useEffect(() => {
-    if (carousel.current) {
-      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
-    }
-  }, []);
-
   return (
     <div className="prueba">
-      <motion.div className="carousel" whileTap={{cursor:"grabbing"}}>
-        <motion.div drag="x" dragConstraints={{right:0, left: -1800}} className="inner-carousel">
-            {images.map((image) => {
-                return (
-                    <motion.div className="text-center item" key={image.id}>
-                        <img className="shadow-lg" src={image.src} alt={image.alt} />
-                        <button className="text-white bg-green-700 hover:bg-[#264133] rounded-l-full rounded-r-full w-[5.5rem] mt-1 font-Roboto-Regular">Más Info.</button>
-                    </motion.div>
-                )
-            })}
+      <motion.div className="carousel" whileTap={{ cursor: "grabbing" }}>
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -1800 }}
+          className="inner-carousel"
+        >
+          {images.map((image, index) => {
+            return (
+              <motion.div className="text-center item" key={image.id}>
+                <Image
+                  className="shadow-lg"
+                  src={image.src}
+                  alt={image.alt}
+                  width={1200}
+                  height={600}
+                />
+                <Link href={image.url}>
+                  <button className="text-white bg-green-700 hover:bg-[#264133] rounded-l-full rounded-r-full w-[5.5rem] mt-1 font-Roboto-Regular">
+                    Más Info.
+                  </button>
+                </Link>
+              </motion.div>
+            );
+          })}
         </motion.div>
       </motion.div>
     </div>
