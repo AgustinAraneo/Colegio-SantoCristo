d79329d11f7deb7ed44255c3868e9caf586d4861
aaraneo <aaraneo@broobe.com>
2024-08-12 15:50:50 -0300

    fix: Update de mail de inscripcion secundaria

--- src/components/ButtonAdmisionSecundario.jsx
+++ src/components/ButtonAdmisionSecundario.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const GmailButton = () => {
-  const recipientEmail = "araneoagustin@gmail.com";
+  const recipientEmail = "secundariasantocristo20@gmail.com";
   const subject = "Inscripción Nivel Secundario";
 
   const handleClick = () => {
@@ -9,7 +9,14 @@ const GmailButton = () => {
     window.open(gmailUrl, "_blank");
   };
 
-  return <button className="bg-blue-500 hover:bg-blue-700 w-[8rem] h-[2.75rem] rounded-l-full rounded-r-full font-Montserrat text-white font-semibold transition duration-500 hover:scale-105" onClick={handleClick} >¡Contactanos!</button>;
+  return (
+    <button
+      className="bg-blue-500 hover:bg-blue-700 w-[8rem] h-[2.75rem] rounded-l-full rounded-r-full font-Montserrat text-white font-semibold transition duration-500 hover:scale-105"
+      onClick={handleClick}
+    >
+      ¡Contactanos!
+    </button>
+  );
 };
 
 export default GmailButton;
