560e00cf6174a2fc079dcab7cd6411b7d5631cb0
AgustinAraneo <araneoagustin@gmail.com>
2024-12-01 18:28:20 -0300

    Funcionaaaa

--- src/pages/novedades/index.jsx
+++ src/pages/novedades/index.jsx
@@ -1,6 +1,6 @@
-import HomeNovedades from "../components/HomeNovedades";
-import FooterContainer from "../components/FooterContainer";
-import HeaderContainer from "../components/HeaderContainer";
+import HomeNovedades from "../../components/HomeNovedades";
+import FooterContainer from "../../components/FooterContainer";
+import HeaderContainer from "../../components/HeaderContainer";
 import React from "react";
 
 const Novedades = () => {
