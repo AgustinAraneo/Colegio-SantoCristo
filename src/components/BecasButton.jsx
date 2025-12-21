eb4ec1d1257b73192d36a8e730bdfa24a1e02068
AgustinAraneo <araneoagustin@gmail.com>
2025-12-21 11:49:30 -0300

    Update becas link

--- src/components/BecasButton.jsx
+++ src/components/BecasButton.jsx
@@ -6,7 +6,7 @@ const BecasButton = () => {
       <Link
         target="_blank"
         rel="noopener noreferrer"
-        href="https://docs.google.com/forms/d/1B3YNPtNea1bgOokEC6GxpX_bISBc9pSEkm2ha8aRKJc/edit"
+        href="https://forms.gle/o6wWCD9Eanwky9vc6"
       >
         <p className="test4 font-Montserrat m-2 xl:text-base lg:text-xs text-[8px] hidden md:block">
           Becas
@@ -16,7 +16,7 @@ const BecasButton = () => {
       <Link
         target="_blank"
         rel="noopener noreferrer"
-        href="https://docs.google.com/forms/d/1B3YNPtNea1bgOokEC6GxpX_bISBc9pSEkm2ha8aRKJc/edit"
+        href="https://forms.gle/o6wWCD9Eanwky9vc6"
       >
         <p className="text-white bg-[#024430] px-10 py-2 rounded-full font-bold text-center text-lg shadow-redcoach-lg block md:hidden">
           Becas
